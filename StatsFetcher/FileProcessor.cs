a7f809c89d1e3ce47aff5eed286f839476758b11
Poma <Semenov.Roman@mail.ru>
2016-03-04 17:17:28 +0300

    Referenced ReplayParser

--- StatsFetcher/FileProcessor.cs
+++ StatsFetcher/FileProcessor.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
+using Heroes.ReplayParser;
+using Foole.Mpq;
 
 namespace StatsFetcher
 {
@@ -18,6 +21,41 @@ namespace StatsFetcher
 				profiles[i].Team = i >= 5 ? 0 : 1;
 			}
 			return profiles;
+		}
+
+		public static void ProcessReplay(string path, List<PlayerProfile> profiles)
+		{
+			var tmpPath = Path.GetTempFileName();
+			File.Copy(path, tmpPath, overwrite: true);
+			var replay = ParseReplay(tmpPath);
+			foreach (var profile in profiles) {
+				var player = replay.Players.Where(p => p.Name == profile.Name).Single();
+				profile.Hero = player.Character;
+				profile.HeroLevel = player.CharacterLevel;
+			}
+			// todo: return replay.Map; replay.GameMode;
+		}
+
+		public static Replay ParseReplay(string fileName)
+		{
+			try {
+				var replay = new Replay();
+
+				var archive = new MpqArchive(fileName);
+				archive.AddListfileFilenames();
+
+				// Replay Details
+				ReplayDetails.Parse(replay, DataParser.GetMpqFile(archive, "save.details"));
+
+				// Player level is stored there
+				ReplayAttributeEvents.Parse(replay, DataParser.GetMpqFile(archive, "replay.attributes.events"));
+
+				return replay;
+			}
+			catch {
+				// todo: eating exceptions is bad
+				return null;
+			}
 		}
 	}
 }
